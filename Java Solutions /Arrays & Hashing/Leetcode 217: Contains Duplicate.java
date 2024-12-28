8e5b6d9da68ad3b8dc0d6af8295e380f6ec76964
mDemarco12 <madams1036@gmail.com>
2024-12-28 11:00:12 -0500

    Creating Solutions to Neet150 in Java to prepare for OA

---
+++ Java Solutions /Arrays & Hashing/Leetcode 217: Contains Duplicate.java
@@ -0,0 +1,21 @@
+
+
+
+public class Solution
+{
+    public boolean hasDuplicate(int[] nums)
+    {
+        for(int i = 0; i < nums.length; i++)
+        {
+            for j =  i; j < nums.length; j++)
+            {
+                if(nums[i] == nums[j]{
+                {
+                    return True;
+                }
+            }
+        }
+        return False
+    }
+
+}
